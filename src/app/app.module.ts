a0f9031c18486a1572facdf35a40008b9569b695
kpekala <kgpekala@gmail.com>
2023-05-30 16:48:40 +0200

    add template and remove test files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FindComponent } from './find/find.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationComponent
+    NavigationComponent,
+    FindComponent
   ],
   imports: [
     BrowserModule,
