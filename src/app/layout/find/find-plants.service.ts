74b4dc072e3426d921603c733cd69ace2733d976
Konrad Pękala <kgpekala@gmail.com>
2023-09-10 17:53:40 +0200

    remove mocked recipes

--- src/app/layout/find/find-plants.service.ts
+++ src/app/layout/find/find-plants.service.ts
@@ -10,36 +10,9 @@ export class FindPlantsService{
 
     private speciesUrl = 'https://my-plants-bd49c-default-rtdb.europe-west1.firebasedatabase.app/species.json';
 
-    private plantTypes: PlantType[] = [
-        {
-            speciesName: 'Humulus',
-            family: 'Cannabaceae',
-            ownersCount: 23,
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Humulus.jpg'
-
-        },
-        {
-            speciesName: 'Dandelion',
-            family: 'Asteraceae',
-            ownersCount: 16,
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Taraxacum_officinale_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-135.jpg/330px-Taraxacum_officinale_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-135.jpg'
-        },
-        {
-            speciesName: 'Oak',
-            family: 'beech',
-            ownersCount: 5,
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Quercus_robur.jpg'
-        }
-    ];
+    private plantTypes: PlantType[] = [];
 
      constructor(private http: HttpClient){
-        http.put(this.speciesUrl, this.plantTypes).subscribe(
-            {next: () => {
-
-            }, error: (error) => {
-                console.log(error.message);
-            }}
-        );
     }
 
     fetchPlants(): Observable<PlantType[]>{
