229e043f9436c431199c6777a8f3b111af761e4c
Konrad Pekala <konrad.pekala@motorolasolutions.com>
2023-09-13 15:10:55 +0200

    unmock plant modal

--- src/app/layout/find/species.model.ts
+++ src/app/layout/find/species.model.ts
@@ -4,6 +4,8 @@ export class Species {
         public speciesName: string,
         public imageUrl: string,
         public ownersCount: number,
-        public family: string) {
+        public family: string,
+        public description: string,
+        public link: string) {
     }
 }
