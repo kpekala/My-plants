89ae1c3031b8ad45778ed77ae77b1c8e08fa968d
Konrad Pękala <kgpekala@gmail.com>
2024-08-26 21:21:29 +0200

    hide firebase api

--- src/app/data/firebase.service.ts
+++ src/app/data/firebase.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from "@angular/core";
-import {FirebaseApp, initializeApp} from "firebase/app"
-import {getFirestore} from "firebase/firestore";
+import { Injectable } from '@angular/core';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { firebaseApi } from 'src/env';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class FirebaseService {
+  firebaseConfig = {
+    apiKey: firebaseApi,
+    authDomain: 'my-plants-bd49c.firebaseapp.com',
+    projectId: 'my-plants-bd49c',
+    storageBucket: 'my-plants-bd49c.appspot.com',
+    messagingSenderId: '995838760830',
+    appId: '1:995838760830:web:7f3809120cd23d3edd852f',
+  };
 
-    firebaseConfig = {
-        apiKey: "AIzaSyDgvYUYfvgpUojN2mCIJT2CIWYXRXnTiNc",
-        authDomain: "my-plants-bd49c.firebaseapp.com",
-        projectId: "my-plants-bd49c",
-        storageBucket: "my-plants-bd49c.appspot.com",
-        messagingSenderId: "995838760830",
-        appId: "1:995838760830:web:7f3809120cd23d3edd852f"
-    };
+  app: FirebaseApp;
+  db: any;
 
-    app: FirebaseApp;
-    db: any;
+  constructor() {
+    this.app = initializeApp(this.firebaseConfig);
+    this.db = getFirestore(this.app);
+  }
 
-    constructor(){
-        this.app = initializeApp(this.firebaseConfig);
-        this.db = getFirestore(this.app);
-    }
-
-    getFirestore(){
-        return this.db;
-    }
+  getFirestore() {
+    return this.db;
+  }
 }
