b00e97be73b328a1f27c5144ca1054a6696be2cf
kpekala <kgpekala@gmail.com>
2024-08-27 13:23:10 +0200

    hotfix

--- src/app/layout/species/species.service.ts
+++ src/app/layout/species/species.service.ts
@@ -1,48 +1,52 @@
-import {Injectable} from "@angular/core";
-import {NewSpecies, Species} from "./species.model";
-import { Observable, Subject, map, switchMap } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { NewSpecies, Species } from './species.model';
+import { Observable, Subject, map, switchMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-export class SpeciesService{
+export class SpeciesService {
+  private speciesUrl =
+    'https://my-plants-bd49c-default-rtdb.europe-west1.firebasedatabase.app/species.json';
 
-    private speciesUrl = 'https://my-plants-bd49c-default-rtdb.europe-west1.firebasedatabase.app/species.json';
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient){
-    }
+  fetchPlants(): Observable<Species[]> {
+    return this.http.get<Species[]>(this.speciesUrl).pipe(
+      map((species: Species[]) => {
+        species = species.filter((s) => s !== null);
+        for (let i = 0; i < species.length; i++) {
+          species[i].id = i;
+        }
+        return species;
+      })
+    );
+  }
 
-    fetchPlants(): Observable<Species[]>{
-        return this.http.get<Species[]>(this.speciesUrl)
-            .pipe(map((species: Species[]) => {
-                for(let i=0; i<species.length; i++) {
-                    species[i].id = i;
-                }
-                return species;
-            }));
-    }
+  updateSpecies(species: Species[]): Observable<any> {
+    return this.http.put(this.speciesUrl, species);
+  }
 
-    updateSpecies(species: Species[]): Observable<any> {
-        return this.http.put(this.speciesUrl, species);
-    }
+  changeSpeciesPopularity(
+    speciesId: number,
+    popularityChange: number
+  ): Observable<any> {
+    let fetchPlantsSub = this.fetchPlants();
+    return fetchPlantsSub.pipe(
+      switchMap((species: Species[]) => {
+        species[speciesId].ownersCount += popularityChange;
+        return this.updateSpecies(species);
+      })
+    );
+  }
 
-    changeSpeciesPopularity(speciesId: number, popularityChange: number): Observable<any>{ 
-        let fetchPlantsSub = this.fetchPlants();
-        return fetchPlantsSub.pipe(
-            switchMap((species: Species[]) => {
-                species[speciesId].ownersCount += popularityChange;
-                return this.updateSpecies(species);
-            })
-        )
-    }
-
-    addSpecies(plant: NewSpecies) {
-        return this.fetchPlants().pipe(
-            switchMap((species: Species[]) => {
-                species.push(plant.mapToSpecies());
-                return this.updateSpecies(species);
-            }
-        ))
-    }
+  addSpecies(plant: NewSpecies) {
+    return this.fetchPlants().pipe(
+      switchMap((species: Species[]) => {
+        species.push(plant.mapToSpecies());
+        return this.updateSpecies(species);
+      })
+    );
+  }
 }
