f1cd4840c0555386e93c6a08fe3f6704be1e8e99
Konrad Pękala <kgpekala@gmail.com>
2024-08-29 17:36:32 +0200

    add garden base component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
-import {LayoutComponent} from "./layout/layout.component";
+import { LayoutComponent } from './layout/layout.component';
 import { AuthComponent } from './auth/auth.component';
 import { homeGuard } from './layout/home/home-guard';
 import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './auth/auth-guard';
 import { profileResolver } from './layout/home/profile/profile-resolver';
 import { SpeciesComponent } from './layout/species/species.component';
+import { GardenComponent } from './layout/garden/garden.component';
 
 const routes: Routes = [
-
-  {path: '', redirectTo: 'app/home', pathMatch: 'full'},
-  {
-	path: 'app',
-	component: LayoutComponent,
-	canActivate: [homeGuard],
-	children: [
-		{path: '', pathMatch: 'full', redirectTo: 'home'},
-		{path: 'home', component: HomeComponent, resolve: [profileResolver]},
-		{path: 'find', component: SpeciesComponent}
-	]
-  },
-  {
-	path: 'auth',
-	component: AuthComponent,
-	canActivate: [authGuard],
-	children: [
-		{path: '', component: LoginComponent, data: {'isLoginView': true}}
-	]
-  },
-  {path: '**', redirectTo: 'auth'}
+    { path: '', redirectTo: 'app/home', pathMatch: 'full' },
+    {
+        path: 'app',
+        component: LayoutComponent,
+        canActivate: [homeGuard],
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'home' },
+            {
+                path: 'home',
+                component: HomeComponent,
+                resolve: [profileResolver],
+            },
+            { path: 'find', component: SpeciesComponent },
+            { path: 'garden', component: GardenComponent },
+        ],
+    },
+    {
+        path: 'auth',
+        component: AuthComponent,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: '',
+                component: LoginComponent,
+                data: { isLoginView: true },
+            },
+        ],
+    },
+    { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
