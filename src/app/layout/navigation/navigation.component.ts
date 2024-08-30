59454d21d630b61ca26f4f4f44485ca38a5592ec
Konrad Pękala <kgpekala@gmail.com>
2024-08-30 16:28:57 +0200

    add bootstrap js file :(

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -7,55 +7,56 @@ import { AuthService } from '../../auth/auth.service';
 import { ProfileService } from '../home/profile/profile.service';
 import { SearchStoreService } from './search.store.service';
 import { ShowCookieStoreService } from 'src/app/config/show-cookie.store.service';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
-  standalone: true,
-  imports: [AppRoutingModule, ReactiveFormsModule],
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss'],
+    standalone: true,
+    imports: [AppRoutingModule, ReactiveFormsModule, NgbDropdownModule],
 })
 export class NavigationComponent implements OnInit {
-  public constructor(
-    private authService: AuthService,
-    private readonly router: Router,
-    private readonly profileService: ProfileService,
-    private readonly searchStoreService: SearchStoreService,
-    private readonly destroyRef: DestroyRef,
-    private readonly showCookieStoreService: ShowCookieStoreService
-  ) {}
+    public constructor(
+        private authService: AuthService,
+        private readonly router: Router,
+        private readonly profileService: ProfileService,
+        private readonly searchStoreService: SearchStoreService,
+        private readonly destroyRef: DestroyRef,
+        private readonly showCookieStoreService: ShowCookieStoreService
+    ) {}
 
-  search = new FormControl('');
+    search = new FormControl('');
 
-  ngOnInit(): void {
-    this.search.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((changes) => {
-        this.searchStoreService.setSearch(changes);
-      });
-  }
-
-  onSignOutClick() {
-    this.authService.signOut();
-  }
-
-  onRemoveAccount() {
-    const shouldRemove = window.confirm(
-      'Are you sure you want to remove your account ?'
-    );
-    if (shouldRemove) {
-      const userToken = this.authService.getUserId();
-      this.authService.removeAccount().then((result) => {
-        this.profileService.deleteProfile(userToken).subscribe({
-          next: () => {
-            this.router.navigate(['/auth']);
-          },
-        });
-      });
+    ngOnInit(): void {
+        this.search.valueChanges
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((changes) => {
+                this.searchStoreService.setSearch(changes);
+            });
     }
-  }
 
-  onShowCookieBanner() {
-    this.showCookieStoreService.setShow();
-  }
+    onSignOutClick() {
+        this.authService.signOut();
+    }
+
+    onRemoveAccount() {
+        const shouldRemove = window.confirm(
+            'Are you sure you want to remove your account ?'
+        );
+        if (shouldRemove) {
+            const userToken = this.authService.getUserId();
+            this.authService.removeAccount().then((result) => {
+                this.profileService.deleteProfile(userToken).subscribe({
+                    next: () => {
+                        this.router.navigate(['/auth']);
+                    },
+                });
+            });
+        }
+    }
+
+    onShowCookieBanner() {
+        this.showCookieStoreService.setShow();
+    }
 }
