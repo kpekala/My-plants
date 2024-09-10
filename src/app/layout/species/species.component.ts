b926f6050ece536e007834fe54918b538cc63173
kpekala <kgpekala@gmail.com>
2024-09-10 11:41:20 +0200

    add toasts to changing collection size and adding/removing from favorites

--- src/app/layout/species/species.component.ts
+++ src/app/layout/species/species.component.ts
@@ -8,6 +8,7 @@ import { ToggleFavEvent } from './plant/plant.component';
 import { Species } from './species.model';
 import { SpeciesService } from './species.service';
 import { SpeciesStoreService } from './species.store.service';
+import { ToastService } from 'src/app/shared/toasts-container/toast.service';
 
 @Component({
     selector: 'app-find-plants',
@@ -29,8 +30,9 @@ export class SpeciesComponent implements OnInit {
         private readonly speciesService: SpeciesService,
         private readonly profileService: ProfileService,
         private readonly searchStoreService: SearchStoreService,
-        readonly profileStoreService: ProfileStoreService,
-        readonly plantsStoreService: SpeciesStoreService
+        private readonly profileStoreService: ProfileStoreService,
+        readonly plantsStoreService: SpeciesStoreService,
+        private readonly toastService: ToastService
     ) {}
 
     ngOnInit(): void {
@@ -99,12 +101,26 @@ export class SpeciesComponent implements OnInit {
             this.profileService.addPlantToFavorites(plantId).subscribe({
                 next: () => {
                     this.reloadSpecies();
+                    this.toastService.success('Plant added to favorites!');
+                },
+                error: () => {
+                    this.toastService.error(
+                        'Failed to add a plant to favorites!'
+                    );
                 },
             });
         } else {
             this.profileService.removePlantFromFavorites(plantId).subscribe({
                 next: () => {
                     this.reloadSpecies();
+                    this.toastService.success(
+                        'Planted removed from favorites!'
+                    );
+                },
+                error: () => {
+                    this.toastService.error(
+                        'Failed to remove a plant from favorites!'
+                    );
                 },
             });
         }
@@ -120,9 +136,13 @@ export class SpeciesComponent implements OnInit {
         this.profileService.changeCollectionSize(id, newSize).subscribe({
             next: () => {
                 this.changeSizeState.set(ChangeSizeState.NOTHING);
+                this.toastService.success(
+                    'Successfully changed collection size!'
+                );
             },
-            error: (err) => {
+            error: () => {
                 this.changeSizeState.set(ChangeSizeState.ERROR);
+                this.toastService.error('Failed to change collection size!');
             },
         });
     }
